9d2d35f49b969c7d815eaafb6528a417f8bcd109
samar <samar63065jeet@gmail.com>
2025-12-07 02:02:14 +0530

    initial commit

--- lib/screens/social/splash_screen.dart
+++ lib/screens/social/splash_screen.dart
@@ -32,7 +32,7 @@ class _SplashScreenState extends State<SplashScreen> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Image.asset(
-              'assets/images/speakflow.png',
+              'assets/images/speakFlow.png',
               width: 150,
               height: 150,
             ),
